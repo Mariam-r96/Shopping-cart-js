fa887f939d8b649fbe54e4f77b0e30fd260906d7
Mariyam-bs23 <mariyam.begom@bs-23.com>
2023-03-12 20:53:04 +0600

    filter search added

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,6 +1,9 @@
 
-let httpRequest , data;
+let httpRequest , data , products_data = [];
+const product_view_row = document.querySelector(".products-page .row");
+const search_field = document.querySelector(".products-page .search-field");
 
+// make api data request 
 let makeRequest = () => {
   httpRequest = new XMLHttpRequest();
   
@@ -12,9 +15,10 @@ let makeRequest = () => {
     let alertContents = () => {
       if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
-          let products = JSON.parse(httpRequest.responseText);
-          viewAllProducts(products);
-          products.map((value) => console.log(value.title))
+          products_data = JSON.parse(httpRequest.responseText);
+          viewAllProducts(products_data);
+          // console.log(products); 
+
         } else {
           alert("There was a problem with the request.");
         }
@@ -27,22 +31,35 @@ let makeRequest = () => {
 }
 
 window.addEventListener("load" , makeRequest);
-const product_view_row = document.querySelector(".products-page .row");
 
+// view all products from the api data 
 const viewAllProducts = (products) => {
     return (product_view_row.innerHTML = products.map((product , key) => {
         return `
-        <div id = product-id-${key} class = "col-md-3">
-          <div class="card mb-4">
+        <div id = product-id-${key} class = "col-md-3 mb-5">
+          <div class="card">
             <div class="card-image">
               <img src=${product.image} class="card-img-top" alt="...">
             </div>
             <div class="card-body">
               <h5 class="card-title">${product.title}</h5>
               <p class="card-text">Price : ${product.price}</p>
+              <button class="btn btn-danger w-100 py-2 d-flex items-center justify-content-center">ADD TO CART <span class="material-symbols-outlined ms-2">
+              shopping_cart</span></button>
             </div>
           </div>
         </div>
             `
     }).join(""))
 }
+
+// filter search 
+search_field.addEventListener("keyup" , e => {
+  const filtered_products = products_data.filter(product =>{
+    if(product.title.toLowerCase().includes(e.target.value.toLowerCase())){
+      return product;
+    }
+  });
+
+  viewAllProducts(filtered_products);
+})
